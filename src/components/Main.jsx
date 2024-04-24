a10abb3b41ffbd59dcedb9b1702e368d1ade9be0
Md Rony <mdronyrsk01724@gmail.com>
2024-04-24 10:53:53 +0600

    post design done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FromAddPost from "./FromAddPost";
+import Post from "./Post";
 
 export default function Main() {
   return (
     <main>
       <FromAddPost />
+      <Post />
     </main>
   );
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Post() {
+  return (
+    <ul className="grid grid-cols-4 gap-8">
+      <li className=" py-4 px-5 border border-[#ffe8cc]">helo</li>
+    </ul>
+  );
+}
